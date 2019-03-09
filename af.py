c4c653420e25c104e6d231cae557f436b5b27e24
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-03-09 23:25:59 +0100

    Add print functionality to finite automaton class

--- af.py
+++ af.py
@@ -3,7 +3,7 @@
 #               Eduardo Marqués De La Fuente
 #               José Carlos Gago Hernández
 # Created:      2019/02/28
-# Last update:  2019/03/06
+# Last update:  2019/03/09
 """Clases y métodos para trabajar con autómatas finitos."""
 
 import copy
@@ -193,6 +193,15 @@ class AF:
         # DUDA: no sé si hay que eliminar los no co-accesibles antes o después
         self._eliminar_estados_no_coaccesibles()
         self._automata_accesible_determinista()
+
+    # Completo
+    def imprimir(self):
+        """Imprime el autómata finito para poder ser visualizado."""
+        print("Estados:",self.estados)
+        print("Alfabeto:",self.alfabeto)
+        print("Transiciones:",self.transiciones)
+        print("Estado inicial:",self.estado_inicial)
+        print("Estados finales:",self.estados_finales)
 
     # Completo
     def copy(self):
